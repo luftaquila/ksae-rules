eb73eedf969581dc3997505fefce00641e286d0e
오병준 <mail@luftaquila.io>
2026-01-29 03:09:20 +0900

    improve: fonts and workflow

--- tex2html.py
+++ tex2html.py
@@ -434,9 +434,6 @@ def create_pandoc_template():
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>$title$</title>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&family=Noto+Serif+KR:wght@400;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="style.css">
   <script>
     MathJax = {
